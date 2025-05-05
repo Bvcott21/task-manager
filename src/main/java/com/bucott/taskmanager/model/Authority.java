ff3f178b100efe91da92aa474f2b941e64f839a3
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-05-05 18:04:52 +0200

    Add Authority enum and Role entity for user role management

---
+++ src/main/java/com/bucott/taskmanager/model/Authority.java
@@ -0,0 +1,6 @@
+package com.bucott.taskmanager.model;
+
+public enum Authority {
+    ROLE_USER,
+    ROLE_ADMIN
+}
--- src/main/java/com/bucott/taskmanager/model/Role.java
+++ src/main/java/com/bucott/taskmanager/model/Role.java
@@ -1,5 +1,22 @@
 package com.bucott.taskmanager.model;
 
-public enum Role {
-    ROLE_USER, ROLE_ADMIN
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Data @NoArgsConstructor @Table(name = "roles")
+public class Role {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+    public Role(Authority authority) {
+        this.authority = authority;
+    }
 }
