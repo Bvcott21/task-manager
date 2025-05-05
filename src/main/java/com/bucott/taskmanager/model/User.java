aa8d60d5f6cf25083c2697fd7c52d68ae46e84db
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-05-05 14:35:38 +0200

    Add Role enum and user roles management in User entity

--- src/main/java/com/bucott/taskmanager/model/User.java
+++ src/main/java/com/bucott/taskmanager/model/User.java
@@ -1,6 +1,14 @@
 package com.bucott.taskmanager.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -16,6 +24,15 @@ public class User {
     private String username;
     private String email;
     private String password;
+
+    @ElementCollection(targetClass = Role.class)
+    @CollectionTable(
+        name = "user_roles", 
+        joinColumns = @JoinColumn(
+            name = "user_id"
+        ))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles = new HashSet<>();
     
     public User(String username, String email, String password) {
         this.username = username;
